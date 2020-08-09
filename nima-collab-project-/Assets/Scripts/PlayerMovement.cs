a826ba6de581d5fe9574e320713ca6834a20aa49
Voldemortailia <zalizadeh780@gmail.com>
2020-08-09 13:39:46 +0430

    Player & gun

--- nima-collab-project-/Assets/Scripts/PlayerMovement.cs
+++ nima-collab-project-/Assets/Scripts/PlayerMovement.cs
@@ -41,6 +41,7 @@ public class PlayerMovement : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+        print(isGrounded);
 
     }
 }
